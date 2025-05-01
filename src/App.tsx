a5ba96aa28ece87c632292fe720271039b2d5554
Aleksandr Lebedev <lebe.dev@outlook.com>
2025-05-01 12:39:01 +0300

    feat: build UI of ErrorMessage

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import Header from "./components/Header";
 import Loader from "./components/Loader";
+import ErrorMessage from "./components/ErrorMessage";
 
 export default function App() {
   const isLoading = false;
+  const isFailed = false;
+
   return (
     <div className="app">
       <Header />
       <main className="app__main">
         {isLoading && <Loader />}
+        {isFailed && <ErrorMessage />}
 
         <h2>Welcome to The React Quiz!</h2>
       </main>
