4760361e003bc3f08b9112865e93b812ab619d5e
Aleksandr Lebedev <lebe.dev@outlook.com>
2025-05-01 11:27:09 +0300

    feat: build UI of Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
+import Header from "./components/Header";
+
 export default function App() {
   return (
     <div className="app">
-      <header>
-        <h1>The React Quiz</h1>
-      </header>
+      <Header />
       <main className="app__main">
         <h2>Welcome to The React Quiz!</h2>
       </main>
