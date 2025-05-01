3643e16e057f1be3eb24a72fe547c46c7bfd7383
Aleksandr Lebedev <lebe.dev@outlook.com>
2025-05-01 11:00:42 +0300

    feat: build app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 export default function App() {
   return (
     <div className="app">
-      <h1>The React Quiz</h1>
+      <header>
+        <h1>The React Quiz</h1>
+      </header>
+      <main className="app__main">
+        <h2>Welcome to The React Quiz!</h2>
+      </main>
     </div>
   );
 }
